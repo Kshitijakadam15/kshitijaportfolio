de9aeb1b446b42a90f19d4bdf183f7e1b9eed342
kshitija <kshsitijakadam219@gmail.com>
2024-11-02 09:11:07 +0530

    new

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,27 +1,28 @@
-
-import { useEffect } from "react"; 
+import { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 
 export const Newsletter = ({ status }) => {
-
   const handleSubmit = (e) => {
     e.preventDefault();
     downloadResume();
-  }
+  };
 
   const downloadResume = () => {
-    const link = document.createElement('a');
-    link.href = process.env.PUBLIC_URL + '/resume/Kshitija_Resume.pdf'; 
-    link.download = 'My_Resume.pdf'; 
+    const link = document.createElement("a");
+    link.href = process.env.PUBLIC_URL + "/resume/Kshitija_Resume.pdf";
+    link.download = "Kshitija_Resume.pdf";
     link.click();
-  }
+  };
 
   return (
     <Col lg={12}>
       <div className="newsletter-bx wow slideInUp">
         <Row>
           <Col lg={12} md={8} xl={6}>
-            <h3>Interested in learning more about my qualifications? Download my resume here.</h3>
+            <h3>
+              Interested in learning more about my qualifications? Download my
+              resume here.
+            </h3>
           </Col>
           <Col md={4} xl={6}>
             <form onSubmit={handleSubmit}>
@@ -33,5 +34,5 @@ export const Newsletter = ({ status }) => {
         </Row>
       </div>
     </Col>
-  )
-}
+  );
+};
